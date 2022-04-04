b36573f220d8fab449637eb13af0a0a9eafd357f
slhscs304 <franklirra@gmail.com>
2022-04-04 12:14:13 -0500

    Finished Token Class

---
+++ src/Token.java
@@ -0,0 +1,26 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Token {
+
+    private int number;
+    private BufferedImage image;
+
+    public Token(int num) {
+        number = num;
+        try {
+            image = ImageIO.read(Token.class.getResource("Token/"+num+".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+}
