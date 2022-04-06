3b1b6ff7548d1396c3963665302d88a36c3d3aeb
Tomin <tchazhikat@gmail.com>
2022-04-05 20:52:13 -0500

    Refactor

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -22,7 +22,7 @@ public class MainMenu extends JFrame {
 
     {
         try {
-            page1 = ImageIO.read(new File("src/Images/CoolPic.jpg"));
+            page1 = ImageIO.read(new File("src/Assets.Images/CoolPic.jpg"));
         } catch (IOException e) {
             e.printStackTrace();
         }
