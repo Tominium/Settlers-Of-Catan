97d6eccdcc0b7c5a9dfeb56784373d612e032557
SlHSCS301 <zmkcanada@gmail.com>
2022-04-08 12:09:59 -0500

    finished Resource Deck

--- src/Panels/InventoryPanel.java
+++ src/Panels/InventoryPanel.java
@@ -1,4 +1,24 @@
 package Panels;
 
+import Cards.DevelopmentCard;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class InventoryPanel {
+    private Player player;
+    private JButton close;
+    private GridBagLayout layout;
+    private GridBagConstraints control;
+    private DevelopmentCard devCard;
+
+    InventoryPanel(Player player) {
+        this.player = player;
+        this.layout = new GridBagLayout();
+        this.control = new GridBagConstraints();
+        this.devCard = new DevelopmentCard();
+    }
+
+    public use
+
 }
--- src/Panels/TradePanel.java
+++ src/Panels/TradePanel.java
@@ -1,4 +1,23 @@
 package Panels;
 
+import javax.swing.*;
+import java.util.TreeMap;
+
 public class TradePanel {
+     Private Player p1;
+     Private Player p2;
+    private TreeMap<String, Integer> Inventory;
+    private TreeMap<String, Integer> p1Trade;
+    private TreeMap<String, Integer> p2Trade;
+    private JButton next;
+    private JButton trade;
+    private String type;
+
+    TradePanel(Player p1, Player p2){
+
+    }
+
+
+
+
 }
